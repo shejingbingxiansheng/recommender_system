47d9c9b8945e65d37aa6baccc15999a7305758fb
shejingbingxiansheng <xiongxiongxiongj@yahoo.com>
2018-11-19 23:08:23 +0800

    fifth

--- rs/model/svd_plus_plus.py
+++ rs/model/svd_plus_plus.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import numpy as np
 
+class SVDPlusPlus:
+    def __init__(self,k,epoch,lr,lam):
+        self.k=k
+        self.epoch = epoch
+        self.lr=lr
+        self.lam=lam
+
+    def init_model(self):
+        print()
